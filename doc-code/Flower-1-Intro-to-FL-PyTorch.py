38962258dfbff8098c6689175c8b7060106486c9
open dev <opendev.top@gmail.com>
2024-04-02 17:35:40 +0800

    Get started with Flower

--- doc-code/Flower-1-Intro-to-FL-PyTorch.py
+++ doc-code/Flower-1-Intro-to-FL-PyTorch.py
@@ -15,9 +15,259 @@ from flwr.common import Metrics
 from flwr_datasets import FederatedDataset
 
 
+''' Step 0: Preparation '''
 # Installing dependencies
 DEVICE = torch.device("cuda")  # Try "cuda" to train on GPU
 print(
     f"Training on {DEVICE} using PyTorch {torch.__version__} and Flower {fl.__version__}"
 )
-disable_progress_bar()
+disable_progress_bar()  # 关闭输出进度条，简洁输出
+
+# Loading the data
+NUM_CLIENTS = 10
+BATCH_SIZE = 32
+
+
+def load_datasets():
+    fds = FederatedDataset(dataset="cifar10", partitioners={"train": NUM_CLIENTS})  # 10个分区代表来自10个组织的不同数据
+
+    def apply_transforms(batch):
+        # Instead of passing transforms to CIFAR10(..., transform=transform)
+        # we will use this function to dataset.with_transform(apply_transforms)
+        # The transforms object is exactly the same
+        transform = transforms.Compose(
+            [
+                transforms.ToTensor(),  # 将图像转换为标量，并将图像数据范围缩放到[0,1]
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),  # 对图像进行标准化处理，将图像的每个通道的像素值转换为均值为 0、标准差为 1 的分布
+            ]
+        )
+        batch["img"] = [transform(img) for img in batch["img"]]
+        return batch
+
+    # Create train/val for each partition and wrap it into DataLoader
+    trainloaders = []
+    valloaders = []
+    for partition_id in range(NUM_CLIENTS):
+        partition = fds.load_partition(partition_id, "train")
+        partition = partition.with_transform(apply_transforms)
+        partition = partition.train_test_split(train_size=0.8)  # 训练总集和验证总集的比例为：8:2
+        trainloaders.append(DataLoader(partition["train"], batch_size=BATCH_SIZE))  # 10个训练集，每个包含4000个训练样本
+        valloaders.append(DataLoader(partition["test"], batch_size=BATCH_SIZE))  # 10个验证集，每个包含1000个训练样本
+    testset = fds.load_split("test").with_transform(apply_transforms)
+    testloader = DataLoader(testset, batch_size=BATCH_SIZE)  # 未分割，包含10000个样本
+
+    return trainloaders, valloaders, testloader
+
+
+trainloaders, valloaders, testloader = load_datasets()
+
+# 查看第一组训练数据（包含1000个样本）中随机的32个图像
+batch = next(iter(trainloaders[0]))
+images, labels = batch["img"], batch["label"]
+# Reshape and convert images to a NumPy array
+# matplotlib requires images with the shape (height, width, 3)
+images = images.permute(0, 2, 3, 1).numpy()
+# Denormalize
+images = images / 2 + 0.5
+
+# Create a figure and a grid of subplots
+fig, axs = plt.subplots(4, 8, figsize=(12, 6))  # 以4行8列输出图像，总尺寸为长1200px宽600px
+
+# Loop over the images and plot them
+for i, ax in enumerate(axs.flat):
+    ax.imshow(images[i])
+    ax.set_title(trainloaders[0].dataset.features["label"].int2str([labels[i]])[0])  # 输出标签
+    ax.axis("off")
+
+# Show the plot
+fig.tight_layout()
+plt.show()
+
+''' Step 1: Centralized Training with PyTorch '''
+
+
+# Defining the model
+class Net(nn.Module):
+    def __init__(self) -> None:
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+# training and test functions
+def train(net, trainloader, epochs: int, verbose=False):
+    """Train the network on the training set."""
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(net.parameters())
+    net.train()
+    for epoch in range(epochs):
+        correct, total, epoch_loss = 0, 0, 0.0
+        for batch in trainloader:
+            images, labels = batch["img"].to(DEVICE), batch["label"].to(DEVICE)
+            optimizer.zero_grad()
+            outputs = net(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            # Metrics
+            epoch_loss += loss
+            total += labels.size(0)
+            correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
+        epoch_loss /= len(trainloader.dataset)
+        epoch_acc = correct / total
+        if verbose:
+            print(f"Epoch {epoch + 1}: train loss {epoch_loss}, accuracy {epoch_acc}")
+
+
+def test(net, testloader):
+    """Evaluate the network on the entire test set."""
+    criterion = torch.nn.CrossEntropyLoss()
+    correct, total, loss = 0, 0, 0.0
+    net.eval()
+    with torch.no_grad():
+        for batch in testloader:
+            images, labels = batch["img"].to(DEVICE), batch["label"].to(DEVICE)
+            outputs = net(images)
+            loss += criterion(outputs, labels).item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    loss /= len(testloader.dataset)
+    accuracy = correct / total
+    return loss, accuracy
+
+
+# Training the model
+trainloader = trainloaders[0]
+valloader = valloaders[0]
+net = Net().to(DEVICE)
+
+for epoch in range(5):
+    train(net, trainloader, 1)
+    loss, accuracy = test(net, valloader)
+    print(f"Epoch {epoch + 1}: validation loss {loss}, accuracy {accuracy}")  # 输出每轮的损失和准确率
+
+loss, accuracy = test(net, testloader)
+print(f"Final test set performance:\n\tloss {loss}\n\taccuracy {accuracy}")  # 输出测试集的测试结果：准确率约为40%
+
+'''Step 2: Federated Learning with Flower'''
+
+
+# Updating model parameters
+def set_parameters(net, parameters: List[np.ndarray]):
+    params_dict = zip(net.state_dict().keys(), parameters)
+    state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
+    net.load_state_dict(state_dict, strict=True)
+
+
+def get_parameters(net) -> List[np.ndarray]:
+    return [val.cpu().numpy() for _, val in net.state_dict().items()]
+
+
+# Implementing a Flower client
+class FlowerClient(fl.client.NumPyClient):
+    def __init__(self, net, trainloader, valloader):
+        self.net = net
+        self.trainloader = trainloader
+        self.valloader = valloader
+
+    def get_parameters(self, config):
+        return get_parameters(self.net)
+
+    def fit(self, parameters, config):
+        set_parameters(self.net, parameters)
+        train(self.net, self.trainloader, epochs=1)
+        return get_parameters(self.net), len(self.trainloader), {}
+
+    def evaluate(self, parameters, config):
+        set_parameters(self.net, parameters)
+        loss, accuracy = test(self.net, self.valloader)
+        return float(loss), len(self.valloader), {"accuracy": float(accuracy)}
+
+
+# Using the Virtual Client Engine
+def client_fn(cid: str) -> FlowerClient:
+    """Create a Flower client representing a single organization."""
+
+    # Load model
+    net = Net().to(DEVICE)
+
+    # Load data (CIFAR-10)
+    # Note: each client gets a different trainloader/valloader, so each client
+    # will train and evaluate on their own unique data
+    trainloader = trainloaders[int(cid)]
+    valloader = valloaders[int(cid)]
+
+    # Create a  single Flower client representing a single organization
+    return FlowerClient(net, trainloader, valloader).to_client()
+
+
+# Starting the training
+# # Create FedAvg strategy
+# strategy = fl.server.strategy.FedAvg(
+#     fraction_fit=1.0,  # Sample 100% of available clients for training
+#     fraction_evaluate=0.5,  # Sample 50% of available clients for evaluation
+#     min_fit_clients=10,  # Never sample less than 10 clients for training
+#     min_evaluate_clients=5,  # Never sample less than 5 clients for evaluation
+#     min_available_clients=10,  # Wait until all 10 clients are available
+# )
+
+# Specify the resources each of your clients need. By default, each
+# client will be allocated 1x CPU and 0x GPUs
+client_resources = {"num_cpus": 1, "num_gpus": 0.0}
+if DEVICE.type == "cuda":
+    # here we are assigning an entire GPU for each client.
+    client_resources = {"num_cpus": 1, "num_gpus": 1.0}
+    # Refer to our documentation for more details about Flower Simulations
+    # and how to setup these `client_resources`.
+
+# # Start simulation
+# fl.simulation.start_simulation(
+#     client_fn=client_fn,
+#     num_clients=NUM_CLIENTS,
+#     config=fl.server.ServerConfig(num_rounds=5),
+#     strategy=strategy,
+#     client_resources=client_resources,
+# )
+
+
+# Where’s the accuracy?
+def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
+    # Multiply accuracy of each client by number of examples used
+    accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
+    examples = [num_examples for num_examples, _ in metrics]
+
+    # Aggregate and return custom metric (weighted average)
+    return {"accuracy": sum(accuracies) / sum(examples)}
+
+# Create FedAvg strategy
+strategy = fl.server.strategy.FedAvg(
+    fraction_fit=1.0,
+    fraction_evaluate=0.5,
+    min_fit_clients=10,
+    min_evaluate_clients=5,
+    min_available_clients=10,
+    evaluate_metrics_aggregation_fn=weighted_average,  # <-- pass the metric aggregation function
+)
+
+# Start simulation
+fl.simulation.start_simulation(
+    client_fn=client_fn,
+    num_clients=NUM_CLIENTS,
+    config=fl.server.ServerConfig(num_rounds=5),
+    strategy=strategy,
+    client_resources=client_resources,
+)
